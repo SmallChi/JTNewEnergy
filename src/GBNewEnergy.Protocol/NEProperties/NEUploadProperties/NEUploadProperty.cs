32bc6a55788ad5dc0b8a2c62c96b6bc74576c5bf
SmallChi <564952747@qq.com>
2018-06-15 00:39:52 +0800

    添加上报信息属性

---
+++ src/GBNewEnergy.Protocol/NEProperties/NEUploadProperties/NEUploadProperty.cs
@@ -0,0 +1,17 @@
+﻿using GBNewEnergy.Protocol.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GBNewEnergy.Protocol.NEProperties.NEUploadProperties
+{
+    /// <summary>
+    /// 上报信息
+    /// </summary>
+    public class NEUploadProperty : INEProperties
+    {
+        public string VIN { get ; set; }
+
+        
+    }
+}
---
+++ src/GBNewEnergy.Protocol/NEProperties/NEUploadProperties/NEUploadPropertyBase.cs
@@ -0,0 +1,12 @@
+﻿using GBNewEnergy.Protocol.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GBNewEnergy.Protocol.NEProperties.NEUploadProperties
+{
+    public abstract class NEUploadPropertyBase
+    {
+        public abstract NEInfoType NEInfoType { get; }
+    }
+}
